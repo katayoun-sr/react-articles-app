3d0f9eb003918f24c503395d7c277664708da6b0
katayoun-sr <katayoun.suroushrad@gmail.com>
2026-02-05 18:00:06 +0330

    style(navbar): update styles and layout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,44 @@
+import { Link } from "react-router-dom";
 import useLang from "../../hooks/useLang";
 
-function Navbar(){
-
-const {texts, setLang} = useLang()
-const langs = texts.navbar
-    return(
-        <nav className="flex justify-between">
-            <ul className="flex ">
-                <li className="p-2">{langs.articlesList}</li>
-                <li className="p-2">{langs.createArticle}</li>
-                <li className="p-2">{langs.aboutUs}</li>
-            </ul>
-
-            <div>
-                <button className="p-2" onClick={() => setLang("fa")}>فارسی</button>
-                <button onClick={() => setLang("en")}>English</button>
-            </div>
-        </nav>
-    )
+function Navbar() {
+  const { texts, setLang } = useLang();
+  const lang = texts.navbar;
+  const menuItems = [
+    { key: lang.articlesList, path: "/" },
+    { key: lang.createArticle, path: "/create-article" },
+    { key: lang.aboutUs, path: "/about-us" },
+  ];
+  return (
+    <nav className="fixed w-full py-5 px-10 text-white">
+      <div className="flex justify-between m-auto">
+        <Link to="/">
+          <img src="/logo.svg" alt="contentify" className="w-45" />
+        </Link>
+        <ul className="flex bg-white/20 backdrop-blur-md py-1 px-4 rounded-full">
+          {menuItems.map((item) => (
+            <li className="py-1 px-6 hover:bg-white/30 cursor-pointer rounded-full">
+              <Link to={item.path}>{item.key}</Link>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <button
+            className="p-2 hover:bg-white/30 rounded-full cursor-pointer"
+            onClick={() => setLang("fa")}
+          >
+            فارسی
+          </button>
+          <button
+            className="p-2 hover:bg-white/30 rounded-full cursor-pointer"
+            onClick={() => setLang("en")}
+          >
+            English
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { LangProvider } from "./context/LangProvider";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
     <LangProvider>
       <App />
     </LangProvider>
-  </StrictMode>,
+    </BrowserRouter>
+  </React.StrictMode>,
 );
