fa98dcd9a1db88411e15a7ac232f7517c6952845
katayoun-sr <katayoun.suroushrad@gmail.com>
2026-02-06 16:15:35 +0330

    feat: add translation files for cards content

--- src/assets/lang/lang.js
+++ src/assets/lang/lang.js
@@ -1,26 +1,30 @@
 import navbarFa from "./fa/navbar.json";
-import footerFa from "./fa/footer.json"
-
-
+import footerFa from "./fa/footer.json";
+import cardFa from "./fa/card.json";
 
 
 import navbarEn from "./en/navbar.json";
-import footerEn from "./en/footer.json"
+import footerEn from "./en/footer.json";
+import cardEn from "./en/card.json";
 
-export const languageList = ['fa', 'en']
+
+
+export const languageList = ["fa", "en"];
 
 const languages = {
-    fa: {
-        dir: "rtl",
-        navbar: navbarFa,
-        footer: footerFa,
-    },
+  fa: {
+    dir: "rtl",
+    navbar: navbarFa,
+    footer: footerFa,
+    card: cardFa,
+  },
 
-    en:{
-        dir: "ltr",
-        navbar: navbarEn,
-        footer: footerEn
-    }
-}
+  en: {
+    dir: "ltr",
+    navbar: navbarEn,
+    footer: footerEn,
+    card: cardEn,
+  },
+};
 
 export default languages;
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import articleImage from "../../assets/images/article.jpg";
+import useLang from "../../hooks/useLang";
 
 function Card() {
+  const {texts} = useLang()
+  const lang = texts.card
   return (
     <article className="flex flex-col  py-4 px-2 h-100 justify-between rounded-2xl overflow-hidden shadow-[0px_8px_6px_-1px_rgba(147,51,234,0.15)] hover:scale-105 border border-gray-300">
       <Link to="">
@@ -14,12 +17,12 @@ function Card() {
         </header>
         <div className=" my-4">
           <p>تست عنوان 1</p>
-          <span>مدت زمان مطالعه: 10 دقیقه</span>
+          <span>{lang.readingTime}: 10 {lang.minute}</span>
         </div>
       </Link>
       <footer className="">
         <button className="cursor-pointer bg-purple-100 py-1 px-4 rounded-full hover:bg-purple-300">
-          ذخیره
+         {lang.save}
         </button>
       </footer>
     </article>
