ec2d5594006e124b2a57a49c2dcfb90153db3a0f
katayoun-sr <katayoun.suroushrad@gmail.com>
2026-02-06 12:46:38 +0330

    style: color palette changed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   const lang = texts.footer;
 
   return (
-    <footer className="h-60 bg-blue-400 w-full fixed bottom-0 text-white ">
+    <footer className="h-60 bg-purple-900 w-full fixed bottom-0 text-white ">
       <div className="max-w-7xl m-auto flex items-center justify-center h-full">
         <h1 className="text-2xl font-bold">{lang.description}</h1>
       </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,27 +10,27 @@ function Navbar() {
     { key: lang.aboutUs, path: "/about-us" },
   ];
   return (
-    <nav className="fixed w-full py-5 px-10 text-white">
+    <nav className="fixed w-full py-2 px-10 backdrop-blur-md shadow-[0px_8px_6px_-1px_rgba(147,51,234,0.15)] text-purple-900">
       <div className="flex justify-between m-auto">
         <Link to="/">
-          <img src="/logo.svg" alt="contentify" className="w-45" />
+          <img src="/logo.svg" alt="contentify" className="w-45 transition-transform duration-300 hover:scale-105" />
         </Link>
-        <ul className="flex bg-white/20 backdrop-blur-md py-1 px-4 rounded-full">
-          {menuItems.map((item) => (
-            <li className="py-1 px-6 hover:bg-white/30 cursor-pointer rounded-full">
+        <ul className="flex bg-purple-100  py-1 px-4 rounded-full">
+          {menuItems.map((item, index) => (
+            <li key={index} className="py-1 px-6 hover:bg-white cursor-pointer rounded-full">
               <Link to={item.path}>{item.key}</Link>
             </li>
           ))}
         </ul>
         <div>
           <button
-            className="p-2 hover:bg-white/30 rounded-full cursor-pointer"
+            className="py-2 px-4 hover:bg-purple-100 rounded-full cursor-pointer"
             onClick={() => setLang("fa")}
           >
             فارسی
           </button>
           <button
-            className="p-2 hover:bg-white/30 rounded-full cursor-pointer"
+            className="py-2 px-4 hover:bg-purple-100 rounded-full cursor-pointer"
             onClick={() => setLang("en")}
           >
             English
