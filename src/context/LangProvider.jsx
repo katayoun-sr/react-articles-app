c656bd7611568f8d79fa3bc3bac17a87c4442583
katayoun-sr <katayoun.suroushrad@gmail.com>
2026-02-07 10:47:18 +0330

    feat: persist language with localStorage

--- src/context/LangProvider.jsx
+++ src/context/LangProvider.jsx
@@ -4,9 +4,15 @@ import languages, { languageList } from "../assets/lang/lang";
 const LangContext = createContext();
 
 export const LangProvider = ({ children }) => {
-  const [lang, setLang] = useState(languageList[0]);
+  const [lang, setLang] = useState(() => {
+    return localStorage.getItem("lang") || languageList[0];
+  });
 
   const texts = languages[lang];
+
+  useEffect(() => {
+    localStorage.setItem("lang", lang);
+  }, [lang]);
 
   useEffect(() => {
     document.documentElement.dir = texts.dir;
